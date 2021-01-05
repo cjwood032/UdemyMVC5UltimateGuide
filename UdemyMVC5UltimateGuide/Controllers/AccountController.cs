c8465fe10a08fc1b9e99f32db12797326fc7fb41
cjwood032 <38894141+cjwood032@users.noreply.github.com>
2021-01-05 10:08:48 -0500

    86. Register page complete

---
+++ UdemyMVC5UltimateGuide/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyMVC5UltimateGuide.Models;
+using UdemyMVC5UltimateGuide.ViewModels;
+using UdemyMVC5UltimateGuide.Identity;
+
+using Microsoft.AspNet.Identity;
+using System.Web.Helpers;
+using Microsoft.Owin.Security;
+namespace UdemyMVC5UltimateGuide.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var appDbContext = new ApplicationDbContext();
+                var userStore = new ApplicationUserStore(appDbContext);
+                var userManager = new ApplicationUserManager(userStore);
+                var passwordHash = Crypto.HashPassword(rvm.Password);
+                var user = new ApplicationUser() { Email = rvm.Email, UserName = rvm.Username, PasswordHash = rvm.Password, 
+                City = rvm.City, Country = rvm.Country, Birthday = DateTime.Parse(rvm.DateOfBirth), Address = rvm.Address, PhoneNumber = rvm.Mobile };
+            IdentityResult result = userManager.Create(user);
+            if(result.Succeeded)
+            {
+                userManager.AddToRole(user.Id, "Customer");
+                var authenticationManger = HttpContext.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManger.SignIn(new AuthenticationProperties(), userIdentity);
+            }
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("My Error", "Invalid data");
+                return View();
+            }
+        }
+    }
+}
---
+++ UdemyMVC5UltimateGuide/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace UdemyMVC5UltimateGuide.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Username cannot be blank")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password cannot be blank")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Passowrd cannot be blank")]
+        [Compare("Password", ErrorMessage ="Passwords do not match") ]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Email cannot be blank")]
+        [EmailAddress(ErrorMessage ="Invalid format for email")]
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
