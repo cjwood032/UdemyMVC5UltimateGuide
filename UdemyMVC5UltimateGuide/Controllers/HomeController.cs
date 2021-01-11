b0825a921d889d081a011492f2f3c8e81cd3180b
cjwood032 <38894141+cjwood032@users.noreply.github.com>
2021-01-11 14:37:30 -0500

    96. Action Filter completed

--- UdemyMVC5UltimateGuide/Controllers/HomeController.cs
+++ UdemyMVC5UltimateGuide/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UdemyMVC5UltimateGuide.Filters;
 
 namespace UdemyMVC5UltimateGuide.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        [MyActionFilter]
         public ActionResult Index()
         {
             return View();
